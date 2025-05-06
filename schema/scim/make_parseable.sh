fc9efeb7c2b062de8a239fcd6d70a2205efbd5bd
Marcus <marcus@hardt-it.de>
2025-05-06 12:20:47 +0200

    convert raw to a valid schema / cim file

---
+++ schema/scim/make_parseable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+cat eduperson_schema_raw.json\
+    | grep -vE "(saml_attribute_name|saml_attribute_urn|status|comment)\":" \
+    | sed s/"#.*"// \
+    | sed s_\ //.*__ \
+    | sed s/\"unclear.*\"/true/ \
+    | sed s/\"to\ be\ evaluated\"/true/ \
+    > eduperson_schema.json
+
+cat eduperson_SCIM_example_raw.json\
+    | grep -vE "(saml_attribute_name|saml_attribute_urn|status|comment)\":" \
+    | sed s/"#\ .*"// \
+    | sed s_\ //.*__ \
+    | sed s/\"unclear.*\"/true/ \
+    | sed s/\"to\ be\ evaluated\"/true/ \
+    > eduperson_SCIM_example.json
